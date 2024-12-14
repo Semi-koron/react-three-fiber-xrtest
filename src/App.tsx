9bdd931112f25a97aa552bc74b87d5230c5708e4
Semi-koron <6jimoto6@gmail.com>
2024-12-14 13:35:31 +0900

    feat VR function

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
           onClick={() => {
             if (!isVR) {
               setIsVR(true);
+              store.enterVR();
               return;
             }
           }}
